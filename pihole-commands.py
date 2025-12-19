be2451c7c44ee8d22d7672fb131ea4049c581c8b
tylermc94 <tylermc94@gmail.com>
2025-12-19 14:08:35 -0500

    Added top_blocked_domains and add_domain
    Able to list the top 10 blocked domains, and add domains to allow or deny lists

--- pihole-commands.py
+++ pihole-commands.py
@@ -1,6 +1,9 @@
 import json
-import requests
+import requests # pyright: ignore[reportMissingModuleSource]
 from config import PIHOLE_ADDR, PIHOLE_API_TOKEN
+import urllib3
+#disable tls warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 sid = ""
 csrf = ""
@@ -42,9 +45,24 @@ def top_domains():
         "X-FTL-CSRF": csrf
     }
     top_domains = requests.request("GET", top_domains_url, headers=headers, verify=False)
-    print(top_domains.json())
 
     output_table(top_domains, "domains", "Domain", "domain")
+
+def top_blocked_domains():
+    global sid
+    global csrf
+    top_domains_url = f"https://{PIHOLE_ADDR}/api/stats/top_domains"
+    params = {
+        "Blocked": True
+    }
+    headers = {
+        "X-FTL-SID": sid,
+        "X-FTL-CSRF": csrf
+    }
+    top_blocked_domains = requests.request("GET", top_domains_url, params=params, headers=headers, verify=False)
+
+    output_table(top_blocked_domains, "domains", "Domain", "domain")
+
 
 def top_clients():
     global sid
@@ -55,18 +73,54 @@ def top_clients():
         "X-FTL-CSRF": csrf
     }
     top_clients = requests.request("GET", top_clients_url, headers=headers, verify=False)
-    print(top_clients.json())
 
     output_table(top_clients, "clients", "Client Name", "name")
 
+def add_domain():
+    global sid
+    global csrf
+    #Create API endpoint URL, requires the type of add
+    print("\nWhat type of domain would you like to add?")
+    print("1. Whitelist")
+    print("2. Blacklist")
+    domain_type_choice = input("\nChoose an option: ")
+    if domain_type_choice == "1":
+        domain_type = "allow"
+    elif domain_type_choice == "2":
+        domain_type = "deny"
+    else:
+        print("Invalid choice!")
+    add_domain_url = f"https://{PIHOLE_ADDR}/api/domains/{domain_type}/exact"
+    #Get domain to be added
+    domain = input("\nDomain to be added:")
+    params = {
+        "domain": domain,
+        "comment": "Added with pihole-commands script",
+        "groups": [
+            0
+        ],
+        "enabled": True
+    }
+    headers = {
+        "X-FTL-SID": sid,
+        "X-FTL-CSRF": csrf
+    }
+    #send request
+    add_domain_request = requests.request("POST", add_domain_url, json=params, headers=headers, verify=False)
+    #confirm success or failure
+    if add_domain_request.status_code == 201:
+        print(f"\nSuccessfully added {domain} to {domain_type} list!")
+    else:
+        print(f"\nFailed to add domain. Status code: {add_domain_request.status_code}")
+        print(add_domain_request.json())  # Show the error message
 
 def show_menu():
     """Display the menu options to the user"""
     print(f"\n=== Connected to: https://{PIHOLE_ADDR} ===")
     print("1. List Top Domains")
     print("2. List Top Clients")
-#    print("3. Remove task")
-#    print("4. Toggle completed")
+    print("3. List Top Blocked Domains")
+    print("4. Add Domain")
 #    print("5. Save list")
 #    print("6. Load list")
 #    print("7. Quit")
@@ -84,10 +138,10 @@ def main():
             top_domains()
         elif choice == "2":
             top_clients()
-#        elif choice == "3":
-#            remove_task(tasks)
-#        elif choice == "4":
-#            toggle_task(tasks)
+        elif choice == "3":
+            top_blocked_domains()
+        elif choice == "4":
+            add_domain()
 #        elif choice == "5":
 #            save_list(tasks)
 #        elif choice == "6":
